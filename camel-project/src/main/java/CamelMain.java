78f3d641e7ae1d5c4072d58a898c8eb4730b1905
Rodrigo Arriaza <arr15334@uvg.edu.gt>
2019-11-15 19:33:51 -0600

    initial commit

---
+++ camel-project/src/main/java/CamelMain.java
@@ -0,0 +1,36 @@
+import org.apache.camel.CamelContext;
+import org.apache.camel.ConsumerTemplate;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CamelMain {
+
+	public static void main(String[] args) throws Exception {
+		CamelContext context = new DefaultCamelContext();
+
+		// adding routes to camel context
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from("direct:start").to("seda:end");
+			}
+		});
+
+		// start the context
+		context.start();
+
+		// producer template
+		ProducerTemplate producerTemplate = context.createProducerTemplate();
+		producerTemplate.sendBody("direct:start", "This message sent by producer");
+
+		// consumer template
+		ConsumerTemplate consumerTemplate = context.createConsumerTemplate();
+		String message = consumerTemplate.receiveBody("seda:end", String.class);
+
+		System.out.println(message);
+
+		// stop the context
+		context.stop();
+	}
+}
